8baf676af97c0a7a09cc2af3cf3b38cccbb6d909
Tanya <REINACREATES1@GMAIL.com>
2022-06-19 09:30:55 -0400

    working api

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const connectionString = process.env.MONGODB_URL
+
+MongoClient.connect(connectionString, {useUnifiedTopology: true})
+const entree = {
+     'dinner':{
+        'salad':'Spring Salad with Burrata',
+        'dessert':'Muscatine Grapes',
+        'drink':'Ginger Orange Spritzer',
+      },
+      'lunch':{
+        'smoothie':'Chocolate Rasberry Smoothie',
+        'dip':'Roasted Red Pepper Hummus',
+        'drink':'Mango Slushie',
+      },
+      'breakfast':{
+        'protein':'Chocolate Rasberry Smoothie',
+        'dip':'Roasted Red Pepper Hummus',
+        'drink':'Mango Slushie',
+      },
+   }
+
+app.get('/', (request, response)=>{
+  response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api',(request,response)=>{
+  response.json(entree)
+})
+
+app.listen(PORT, ()=>{
+  console.log('The server is running on port ${PORT}! You better go catch it!')
+})
